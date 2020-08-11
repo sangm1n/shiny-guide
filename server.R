2d26ac161c5e6c68d5c157efbe0da1a756cc7fb7
Lee Sang Min <dltkd96als@naver.com>
2020-08-11 19:25:49 +0900

    Initialize server.R file

---
+++ server.R
@@ -0,0 +1,16 @@
+library(shiny)
+library(broom)
+
+function(input, output, session) {
+    output$textOut <- renderPrint({ input$text })
+    output$numOut <- renderPrint({ input$num })
+    output$slide1Out <- renderPrint({ input$slide1 })
+    output$slide2Out <- renderPrint({ input$slide2 })
+    output$slide3Out <- renderPrint({ input$slide3 })
+    output$selectOut <- renderPrint({ input$select })
+    output$radioOut <- renderPrint({ input$radio })
+    output$checkOut <- renderPrint({ input$check })
+    output$tableOut <- renderTable({ tidy(lm(mpg~wt+qsec, data=mtcars ))},
+                                   striped=TRUE, hover=TRUE, bordered=TRUE)
+    output$plotOut <- renderPlot({ plot(mtcars$wt, mtcars$mpg, col="red") })
+}
